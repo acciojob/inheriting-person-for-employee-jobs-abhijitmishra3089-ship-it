ba8dd14a3ab6901c7fa920041d13a0fd279367be
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-13 16:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
-// complete this js code
-function Person(name, age) {
-		this.name=name;
-		this.age=age;
-}
-	Person.prototype.greet=function(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
+// // complete this js code
+// function Person(name, age) {
+// 		this.name=name;
+// 		this.age=age;
+// }
+// 	Person.prototype.greet=function(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+// 	}
 
+
+// function Employee(name, age, jobTitle) {
+// 	Person.call(this,name,age);
+// 		this.jobTitle=jobTitle;
+// }
+// 	Employee.prototype=Object.create(Person.prototype);
+// 	Employee.prototype.constructor=Employee;
+// 	Employee.protptype.jobGreet=function(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+// 	}
+
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.greet = function () {
+    console.log(
+        `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+};
 
 function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-		this.jobTitle=jobTitle;
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
 }
-	Employee.prototype=Object.create(Person.prototype);
-	Employee.prototype.constructor=Employee;
-	Employee.protptype.jobGreet=function(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
+
+// Set up inheritance
+Employee.prototype = Object.create(Person.prototype);
+
+// Fix constructor reference
+Employee.prototype.constructor = Employee;
+
+// Add method
+Employee.prototype.jobGreet = function () {
+    console.log(
+        `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+};
 
 // Do not change code below this line
 window.Person = Person;
