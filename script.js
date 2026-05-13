d4c118f53013e7620f50c736307e724a19154971
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-13 14:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 // complete this js code
 function Person(name, age) {
-	consturctor(name,age){
 		this.name=name;
 		this.age=age;
-	}
-	greet(){
+	Person.prototype.greet=function(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 	}
 }
 
 function Employee(name, age, jobTitle) {
-	consturctor(name,age,jobTitle){
-		super(name,age);
 		this.jobTitle=jobTitle;
-	}
-	jobGreet(){
+	Employee.prototype=Object.create(Person.prototype);
+	Employee.prototype.constructor=Employee;
+	Employee.protptype.jobGreet=function(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
