d418fe8d5ca4cd9171037ca8cd7f2ea23a06128b
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-05-13 15:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ function Person(name, age) {
 }
 
 function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
 		this.jobTitle=jobTitle;
+}
 	Employee.prototype=Object.create(Person.prototype);
 	Employee.prototype.constructor=Employee;
 	Employee.protptype.jobGreet=function(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
-}
 
 // Do not change code below this line
 window.Person = Person;
